44d04b668a126cc733c5958c660f134e140589ca
Alex Poletavkin <poletavin.av@gmail.com>
2024-10-08 15:50:22 +0200

    init

---
+++ Lesson08_ChatClient/src/ait/chat/client/ChatClientAppl.java
@@ -0,0 +1,35 @@
+package ait.chat.client;
+
+import ait.chat.client.task.MessageReceiver;
+import ait.chat.client.task.MessageSender;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ChatClientAppl {
+    public static void main(String[] args) throws InterruptedException {
+        String serverHost = "127.0.0.1";
+        int port = 9000;
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        try {
+            Socket socket = new Socket(serverHost, port);
+             MessageSender messageSender = new MessageSender(socket);
+             MessageReceiver messageReceive = new MessageReceiver(socket);
+
+            executorService.execute(messageReceive);
+            executorService.execute(messageSender);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            executorService.shutdown();
+            executorService.awaitTermination(1, TimeUnit.MINUTES);
+        }
+
+
+    }
+}
