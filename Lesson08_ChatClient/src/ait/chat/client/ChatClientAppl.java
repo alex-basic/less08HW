4ecd4bd9184cb9edc7c3dd2acee8fe2b0422aa68
Alex Poletavkin <poletavin.av@gmail.com>
2024-10-08 21:49:26 +0200

    finished

--- Lesson08_ChatClient/src/ait/chat/client/ChatClientAppl.java
+++ Lesson08_ChatClient/src/ait/chat/client/ChatClientAppl.java
@@ -2,7 +2,6 @@ package ait.chat.client;
 
 import ait.chat.client.task.MessageReceiver;
 import ait.chat.client.task.MessageSender;
-
 import java.io.IOException;
 import java.net.Socket;
 
@@ -11,9 +10,9 @@ public class ChatClientAppl {
         String serverHost = "127.0.0.1";
         int port = 9000;
 
-        Socket socket = null;
         try {
-            socket = new Socket(serverHost, port);
+            Socket socket = new Socket(serverHost, port);
+
             Thread taskMessageSender = new Thread(new MessageSender(socket));
             taskMessageSender.setDaemon(true);
 
@@ -23,6 +22,7 @@ public class ChatClientAppl {
             taskMessageReceiver.start();
             taskMessageSender.start();
             taskMessageSender.join();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
